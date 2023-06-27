7ae14baf5d54cab3c51e16b8dcb8d7254be84d36
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-06-27 16:13:03 +0200

    added backend's calls for generating emails

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,23 @@
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
+
 const app = Vue.createApp({
     data() {
         return {
             emails: [],
         }
     },
-});
+    created() {
+        for (let i = 0; i < 10; i++) {
+          axios.get(endpoint)
+            .then((res) => {
+              const resultEmail = res.data.response;
+              this.emails.push(resultEmail);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      },
+    });
 
 app.mount('#root');
